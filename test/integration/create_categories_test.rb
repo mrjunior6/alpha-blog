9833fd67aefcb02428fd68d30af1598e368085cb
Helio Junior <helio.jr@icloud.com>
2017-10-09 09:41:28 -0300

    Add categories model and tests

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -18,10 +18,9 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
 		assert_template 'categories/new'
 		assert_no_difference 'Category.count' do
 			post categories_path, params: { category: { name: " "}}
-			
 		end
 		assert_template 'categories/new'
 		assert_select 'h2.panel-title'
-		assert_select 'div.panel-title'
+		assert_select 'div.panel-body'
 	end
 end
